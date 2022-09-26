4498f7fb2dcba82d667480bbe2aed266e4bb51c0
Juan Gabriel <69330692+juan192001-jg@users.noreply.github.com>
2022-09-26 15:26:29 -0500

    base de datos

--- Database.sql
+++ Database.sql
@@ -3,7 +3,7 @@ CREATE DATABASE infomedica;
 USE infomedica;
 
 CREATE TABLE usuarios (
-    id_us INT(10) AUTO_INCREMENT NOT NULL,
+    id_us INT(10) NOT NULL,
     username_us VARCHAR(20)NOT NULL,
     password VARCHAR(20)NOT NULL,
     fullname_us VARCHAR(40)NOT NULL,
@@ -11,21 +11,21 @@ CREATE TABLE usuarios (
     celular_us INT (10)NOT NULL,
     documento_us INT(10)NOT NULL,
     tipousuario VARCHAR(10)NOT NULL,
-    estado_us VARCHAR(10)NOT NULL,
-    created_at timestamp NOT NULL DEFAULT current_timestamp
+    estado_us VARCHAR(10)NOT NULL
+    PRIMARY KEY (id_us);
 );
-ALTER TABLE usuarios
-  ADD PRIMARY KEY (documento_us);
+
+
+USE infomedica;
 
 CREATE TABLE especialidad_medica (
     id INT(10) AUTO_INCREMENT NOT NULL,
     medicoid INT(10)NOT NULL,
     descripcion VARCHAR(100) NOT NULL,
-    created_at timestamp NOT NULL DEFAULT current_timestamp,
-    CONSTRAINT fk_medicoid FOREIGN KEY(medicoid) REFERENCES usuarios(documento_us)
+    PRIMARY KEY (id),
+    CONSTRAINT fk_medicoid FOREIGN KEY (medicoid) REFERENCES usuarios(id_us)
 );
-ALTER TABLE especialidad_medica
-  ADD PRIMARY KEY (id);
+USE infomedica;
 CREATE TABLE citas (
    id_ct INT(10) AUTO_INCREMENT NOT NULL,
    id_pc INT (10)NOT NULL,
@@ -33,14 +33,13 @@ CREATE TABLE citas (
    fecha_ct DATE NOT NULL,
    hora_ct NOT NULL,
    edad_ct INT(2) NOT NULL,
-   created_at timestamp NOT NULL DEFAULT current_timestamp,
-   CONSTRAINT fk_id_pc FOREIGN KEY(id_pc) REFERENCES usuarios(documento_us),
-   CONSTRAINT fk_id_m  FOREIGN KEY(id_m) REFERENCES usuarios(documento_us)
+   PRIMARY KEY (id_ct),
+   CONSTRAINT fk_id_pc FOREIGN KEY(id_pc) REFERENCES  usuarios(id_us),
+   CONSTRAINT fk_id_m  FOREIGN KEY(id_m) REFERENCES  usuarios(id_us)
 
 );
-ALTER TABLE citas
-  ADD PRIMARY KEY (id_ct);
 
+USE infomedica;
 CREATE TABLE historial(
     id_hs INT(10) AUTO_INCREMENT NOT NULL,
     id_pc INT (10)NOT NULL,
@@ -54,12 +53,9 @@ CREATE TABLE historial(
     ocupacionPac_hs VARCHAR(20)NOT NULL,
     observacionesPas_hs VARCHAR(150)NOT NULL,
     medicamentos_hs VARCHAR(100)NOT NULL,
-    created_at timestamp NOT NULL DEFAULT current_timestamp,
-    CONSTRAINT fk_id_m FOREIGN KEY(id_m) REFERENCES usuarios(documento_us),
-    CONSTRAINT fk_id_pc FOREIGN KEY(id_pc) REFERENCES usuarios(documento_us),
+    PRIMARY KEY (id_hs),
+    CONSTRAINT fk_id_me FOREIGN KEY(id_m) REFERENCES  usuarios(id_us),
+    CONSTRAINT fk_id_pa FOREIGN KEY(id_pc) REFERENCES  usuarios(id_us),
     CONSTRAINT fk_id_ct FOREIGN KEY(id_ct) REFERENCES citas(id_ct)
    );
-
-
-   ALTER TABLE historial
-  ADD PRIMARY KEY (id_hs);
+ 
